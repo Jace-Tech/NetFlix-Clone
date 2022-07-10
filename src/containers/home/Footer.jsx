22fb35a02e56630dbd1dd2d5fa547ae154f94d79
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-07-10 06:22:22 +0100

    done with the Home page animation

--- src/containers/home/Footer.jsx
+++ src/containers/home/Footer.jsx
@@ -14,51 +14,57 @@ const Footer = () => {
         setFooterLinks(data.footer.links)
     }
 
+    // GET USER'S IP ADDRESS
     const handleFetchIpAddress = async () => {
         const response = await fetch('https://api.ipify.org?format=json')
         const data = await response.json()
-        console.log({data})
+        console.log({ data })
         handleFetchIpData(data.ip)
     }
 
+    // GET USER'S LOCATION INFO USING IT'S IP ADDRESS
     const handleFetchIpData = async (address) => {
         // const response = await fetch(`https://ipapi.co/json/`)
         const response = await fetch(`http://ip-api.com/json/${address}`)
         const data = await response.json()
-       setCountry(data.country)
+        setCountry(data.country)
     }
 
     useEffect(() => {
         handleGetFooterLinks()
         handleFetchIpAddress()
     }, [])
+    
     return (
         <Box py={{ base: 20, sm: 16, lg: 12 }} bgColor={"black"}>
             <Container maxW={"container.xl"} w={"90%"}>
-                <Box>
+                <div data-aos={"fade-in"} data-aos-delay={"50"}>
                     <Link fontSize={"md"} color={"whiteAlpha.500"}>Questions? Contact us.</Link>
-                </Box>
+                </div>
 
                 <SimpleGrid columns={[1, 2, 3, 4]} spacing={8} mt={5}>
                     {
                         footerLinks && footerLinks.map((links, index) => (
-                            <List spacing={3} key={index}>
-                                {links.map(({ external, to, name }, _index) => (
-                                    <ListItem fontSize={"sm"} fontWeight={"light"} color={"whiteAlpha.500"} key={`${index}-${_index}`}>
-                                        <Link  as={ external ? "a" : ReactLink } href={ external && to } to={ !external && to }>{ name }</Link>
-                                    </ListItem>
-                                ))}
-                            </List>
+                            <div data-aos={"fade-up"} data-aos-duration={"2000"} data-aos-delay={`${index * 100}`} key={index}>
+                                <List spacing={3} >
+                                    {links.map(({ external, to, name }, _index) => (
+                                        <ListItem fontSize={"sm"} fontWeight={"light"} color={"whiteAlpha.500"} key={`${index}-${_index}`}>
+                                            <Link as={external ? "a" : ReactLink} href={external && to} to={!external && to}>{name}</Link>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </div>
+
                         ))
                     }
                 </SimpleGrid>
 
                 <Box mt={8}>
-                    <Text fontSize={"sm"} fontWeight={"semibold"} color={"whiteAlpha.500"}>Netflix { country }</Text>
+                    <Text fontSize={"sm"} fontWeight={"semibold"} color={"whiteAlpha.500"}>Netflix {country}</Text>
                 </Box>
             </Container>
         </Box>
     )
-}   
+}
 
 export default Footer

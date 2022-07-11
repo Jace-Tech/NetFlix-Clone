b3d109da160eefde77a14602ba6574d77584c777
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-07-11 11:00:04 +0100

    Fixed scroll to top bug

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect } from 'react'
 
 const ScrollTop = ({ children }) => {
-    const handleScrollTop = () => {
-        window.scrollTo({ top: 0, left: 0,  behavior: 'smooth' })
-    }
 
     useEffect(() => {
-        const handler = window.addEventListener('scroll', handleScrollTop, { once: true })
-        return () => window.removeEventListener('scroll', handler)
+        window.scrollTo({ top: 0, left: 0,  behavior: 'smooth' })
     }, [])
 
     return  children
